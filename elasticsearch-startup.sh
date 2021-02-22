aeffac91ce83d6b7a3dab797e3983d2a514e7465
Luigi Selmi <luigiselmi@gmail.com>
2021-02-22 11:51:52 +0200

    minor change

--- elasticsearch-startup.sh
+++ elasticsearch-startup.sh
@@ -7,7 +7,7 @@ echo `date` $0
 ( # run concurrent to elasticsearch:
     while ! ( ./healthcheck ) ;do echo expect to become healthy; sleep 5; done
     echo XXX $0 initialisation finished, service is healthy
-    curl -H "Content-Type: application/json" -XPUT "http://localhost:9200/thessaloniki?include_type_name=true"  -d @thessaloniki-settings.json
+    curl -XPUT "http://localhost:9200/thessaloniki?include_type_name=true" -H "Content-Type: application/json" -d @thessaloniki-settings.json
     echo XXX $0 index with mapping created
 ) &
     
